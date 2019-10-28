439fdc640b6132974d7800ad2b68bff6c83e54f0
Kyle Husmann <kyle.husmann@gmail.com>
2019-10-28 18:25:35 -0400

    start moving to new data system

--- scripts/make_mmod.R
+++ scripts/make_mmod.R
@@ -1,14 +1,14 @@
-source('scripts/eclsk2011data.R')
+source('scripts/eclsk2011data2.R')
+source('scripts/mxMmodModel.R')
 
-make_mmod <- function(data, measures, occasions, name, fiml, outdir) {
+make_mmod <- function(data, measures, name, fiml, outdir) {
   force(data)
   force(measures)
-  force(occasions)
   force(name)
   force(fiml)
   force(outdir)
   quo(
-    mmodModel(eclsk2011data[[data]], measures, occasions, name, fiml) %>%
+    mxMmodModel(filter(eclsk2011[[data]], split=='test'), name, idvar='CHILDID', timevar='occasions', measures, fiml) %>%
     mxOption('Checkpoint Directory', outdir) %>%
     mxOption('Checkpoint Prefix', paste0(name, '_')) %>%
     mxRestore(chkpt.directory=outdir, chkpt.prefix=paste0(name, '_'))
@@ -17,7 +17,6 @@ make_mmod <- function(data, measures, occasions, name, fiml, outdir) {
 
 make_mmod(data=snakemake@params[['data']],
           measures=snakemake@params[['measures']],
-          occasions=snakemake@params[['occasions']],
           name=snakemake@wildcards[['model']], 
           fiml=snakemake@config[['FIML']],
           outdir=dirname(snakemake@output[[1]])) %>% write_rds(snakemake@output[[1]])
