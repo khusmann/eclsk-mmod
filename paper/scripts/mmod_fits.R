d35cdcb271a2b1adf30bc0567e1593c64e4ed266
Kyle Husmann <kyle.husmann@gmail.com>
2020-02-12 16:59:44 -0500

    reformat mmod fits table

--- paper/scripts/mmod_fits.R
+++ paper/scripts/mmod_fits.R
@@ -26,38 +26,33 @@ study1_results <- mmod_results('eclsk2011_study1')
 study1_fits <- mmod_fits(study1_results)
 
 study1_fits %>%
-  mutate(name = fct_relevel(name, 'theory', after=0)) %>%
+  mutate(name = fct_relevel(name, 'theory', after=Inf)) %>%
   arrange(name) %>%
-  extract(name, c('numFct', 'occ'), 'efa(.)occ(.+)') %>%
+  extract(name, c('numFct', 'occ'), 'efa(.)occ(.+)') %>% 
+  mutate(occ = map(str_split(occ, pattern=''), as.numeric)) %>% 
+  unnest(occ) %>%
   mutate(occ = map_chr(str_split(occ, ''), str_c, collapse = ', ')) %>%
-  mutate(occ = str_replace(occ, '1, 2, 4', 'All')) %>%
   mutate(occ = str_replace(occ, '1', 'Fall Kindergarten')) %>%
   mutate(occ = str_replace(occ, '2', 'Spring Kindergarten')) %>%
   mutate(occ = str_replace(occ, '4', 'Spring 1st Grade')) %>%
-  mutate(numFct = replace_na(numFct, 3),
-         occ = replace_na(occ, '-')) %>%
+  mutate(numFct = replace_na(numFct, 3)) %>%
+  filter(numFct != '5') %>%
+  mutate(numFct = str_c(numFct, '-Factor'),
+         occ = replace_na(occ, 'Theory')) %>%
   mutate_at(vars(n:bic), ~sprintf('%0.0f', .)) %>%
   mutate_at(vars(rmsea:tli), ~sprintf('%0.3f', .)) %>%
-  filter(numFct != '5') %>%
-  select(-numFct) %>%
-  rename(`Occasions With Same Structure (from training set)` = occ,
-         `N` = n,
-         `Chisq` = chisq,
-         `df` = dof,
-         `-2LL` = `-2ll`,
-         `AIC` = aic,
-         `BIC` = bic,
-         rmsea = rmsea,
-         cfi = cfi,
-         tli = tli) %>%
-  kable('latex', caption='MMOD Model Fit Statistics of Candidate Factor Structures',
-        label='mmod_fits', booktabs=T, align=c('l', rep('l', 10)), escape=F) %>%
+  mutate_at(vars(n:tli), ~cell_spec(., 'latex', bold = row_number() %in% c(7,8))) %>%
+  transmute(`Structure Type` = numFct,
+            `Structure Source` = occ,
+            AIC = aic,
+            BIC = bic,
+            rmsea = rmsea,
+            cfi = cfi,
+            tli = tli) %>%
+  kable('latex', caption='MMOD Model Fit Statistics of Candidate Factor Structures on Test Subset',
+        label='mmod_fits', booktabs=T, escape=F) %>%
   kable_styling(latex_options = c('scale_down')) %>%
-  row_spec(1, bold = T) %>%
-  row_spec(5, bold = T) %>%
-  group_rows(index=c('Theoretical Structure (3 Factors)' = 1,
-                     'EFA 2 Factor' = 1,
-                     'EFA 3 Factor' = 2,
-                     'EFA 4 Factor' = 2), latex_gap_space='2em') %>%
-  add_header_above(c(' ' = 1, 'MMOD Model Fit Statistics (on test set)' = 9)) %>%
+  collapse_rows(1:7) %>%
+  footnote(general='The MMOD-selected factor structure was the 4-Factor Structure found in both the Fall and Spring of Kindergarten measurement occasions (fit statistics in bold)',
+           threeparttable=T) %>%
   save_latex_table('mmod_fits')
