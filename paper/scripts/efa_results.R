5ee9de8abe71d32a5f485a7564c4caaf2aebeb72
Kyle Husmann <kyle.husmann@gmail.com>
2020-02-22 10:46:57 -0500

    move data generation into efa

--- paper/scripts/efa_results.R
+++ paper/scripts/efa_results.R
@@ -8,6 +8,88 @@ occasion_list <- c(
   `Spring of 1st Grade` = 4
 )
 
+onames <- c(
+  `1` = 'Fall of Kindergarten',
+  `2` = 'Spring of Kindergarten',
+  `4` = 'Spring of 1st Grade'
+)
+
+fnames <- list(
+  `2` = c('ATL+AF+IC', 'BE'),
+  `3` = c('ATL', 'AF+IC', 'BE'),
+  `4` = c('ATL', 'AF', 'IC', 'BE'),
+  `5` = c('ATL', 'AF', 'AF2', 'IC', 'BE')
+)
+
+filter_occasion <- function(data, o) {
+  data %>%
+    filter(occasion == o) %>%
+    select(-occasion) %>%
+    na.omit()
+}
+
+study1_explore <- function(occasion, nfactors) {
+  require(psych)
+  fsort_order <- list(
+    `2` = list(
+      `1` = c(1,2),
+      `2` = c(1,2),
+      `4` = c(1,2)
+    ),
+    `3` = list(
+      `1` = c(2,1,3),
+      `2` = c(2,1,3),
+      `4` = c(1,2,3)
+    ),
+    `4` = list(
+      `1` = c(1,2,3,4),
+      `2` = c(1,3,2,4),
+      `4` = c(1,2,3,4)
+    ),
+    `5` = list(
+      `1` = c(1,5,2,3,4),
+      `2` = c(1,5,4,2,3),
+      `4` = c(2,3,5,1,4)
+    )   
+  )
+  
+  freflects <- list(
+    `2` = list(
+      `1` = c(),
+      `2` = c(),
+      `4` = c()
+    ),
+    `3` = list(
+      `1` = c(),
+      `2` = c(),
+      `4` = c()
+    ),
+    `4` = list(
+      `1` = c(2),
+      `2` = c(2),
+      `4` = c(2)
+    ),
+    `5` = list(
+      `1` = c(2,3),
+      `2` = c(2,3),
+      `4` = c(2,3)
+    )   
+  )
+  
+  measures <- c('TKEEPS', 'TSHOWS', 'TWORKS', 'TADAPTS', 'TFOLLOW', 'TPERSIS', 'TATTEN',
+                'TBEZDAC', 'TBNOFIN', 'TBGCCLR', 'TBGCBLD', 'TBEZDSL', 'TBABSBK',
+                'TBWTTSK', 'TBTRBST', 'TBFLWIN', 'TBSTNO') # Omit: 'TBPLNAC', 'TBAPRRK'
+
+  eclsk2011$study1 %>%
+    filter(split == 'train') %>%
+    select(c('occasion', measures)) %>%
+    filter_occasion(occasion) %>%
+    fa(nfactors, rotate='promax') %>%
+    `$<-`('score.cor', matrix(nrow=nfactors,ncol=nfactors)) %>% # TODO: this is a workaround for a fa.organize bug
+    fa.organize(o=fsort_order[[as.character(nfactors)]][[as.character(occasion)]]) %>%
+    reflect(freflects[[as.character(nfactors)]][[as.character(occasion)]])
+}
+
 make_loadings_table <- function(nfactors, foot = NULL) {
   map(occasion_list, function(occasion) {
       study1_explore(occasion, nfactors) %>%
@@ -78,6 +160,6 @@ make_correlations_table(4, 4)
 
 # 5-Factor Structures
 make_loadings_table(5, footer)
-ake_correlations_table(1, 5)
+make_correlations_table(1, 5)
 make_correlations_table(2, 5)
 make_correlations_table(4, 5)
