70e268860d5c093fc2630c43f69f3e09cf1a9061
Kyle Husmann <kyle.husmann@gmail.com>
2020-02-23 12:27:44 -0500

    add snakemake version to table output

--- scripts/eclsk2011_study1_tables/version_info.R
+++ scripts/eclsk2011_study1_tables/version_info.R
@@ -1,8 +1,9 @@
-library(tidyverse)
+source('scripts/eclsk2011_study1_tables/common.R')
 library(OpenMx)
 
 cat(
   sprintf('\\newcommand\\rversion{%s}\n', R.version.string),
   sprintf('\\newcommand\\mxversion{%s}\n', mxVersion()),
+  sprintf('\\newcommand\\snakeversion{%s}\n', system('snakemake --version', intern=T)),
   file=file.path(OUTDIR, 'version_info.tex')
 )
