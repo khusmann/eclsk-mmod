a0aaa9533339c22c56ebe59c9d717baf2b0071d5
Kyle Husmann <kyle.husmann@gmail.com>
2020-02-23 22:06:12 -0500

    remove snakemake version

--- scripts/eclsk2011_study1_tables/version_info.R
+++ scripts/eclsk2011_study1_tables/version_info.R
@@ -4,6 +4,5 @@ library(OpenMx)
 cat(
   sprintf('\\newcommand\\rversion{%s}\n', R.version.string),
   sprintf('\\newcommand\\mxversion{%s}\n', mxVersion()),
-  sprintf('\\newcommand\\snakeversion{%s}\n', system('snakemake --version', intern=T)),
   file=file.path(OUTDIR, 'version_info.tex')
 )
